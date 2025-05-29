ee642c0564fd3d5e9c13e7cc3828cc7fb689f122
Price Simulator Dev <dev@price-simulator.local>
2025-05-29 09:02:28 -0400

    test: update conftest.py
    - Update backend/tests/conftest.py (+21/-0): client function, override_get_db function

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -10,3 +10,24 @@ from app.main import app
 SQLALCHEMY_DATABASE_URL = "sqlite://"
 
 
+@pytest.fixture()
+def client():
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL,
+        connect_args={"check_same_thread": False},
+        poolclass=StaticPool,
+    )
+    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    def override_get_db():
+        db = TestingSessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    with TestClient(app) as c:
+        yield c
+    app.dependency_overrides.clear()
